a7212774b13d65851c5d056273c359afb2a54856
JRoTheGeek <jvrosenman@gmail.com>
2013-09-02 21:43:54 -0400

    Basic Phase 1 functionality. Still need to add statistics.

--- Project1/runCommand.c
+++ Project1/runCommand.c
@@ -1,8 +1,27 @@
 // Kristen Brann & Jason Rosenman
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-int main(int argc, char[] argv) {
-	
-	return 0;
+int main(int argc, char** argv) {
+	int status, pid;
+	pid = fork();
+	if(pid == 0) {
+		int error = execvp(argv[0], ++argv);
+		if(error == -1) {
+			if(errno == ENOENT) {
+					printf("Filename or path not found.\n");
+					return errno;
+			}
+			else return errno;
+		}
+	}
+	wait(&status);
+	printf("Child %d died with status %d.\n", pid, status);
+	return status;
 }
