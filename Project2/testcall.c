15d7b623a608601c27a63b7818906f670d987f85
Operating Systems <os@Bert.(none)>
2013-09-16 01:00:43 -0400

    Phase 1 complete.

---
+++ Project2/testcall.c
@@ -0,0 +1,16 @@
+#include <sys/syscall.h>
+#include <stdio.h>
+// These values MUST match the unistd_32.h modifications:
+#define __NR_cs3013_syscall1 349
+#define __NR_cs3013_syscall2 350
+#define __NR_cs3013_syscall3 351
+
+long testCall1 ( void) {
+	return (long) syscall(__NR_cs3013_syscall1);
+}
+
+int main () {
+	printf("The return values of the system calls are:\n");
+	printf("\tcs3013_syscall1: %ld\n", testCall1());
+	return 0;
+}
