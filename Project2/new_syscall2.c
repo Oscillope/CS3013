64cb9f4bc55d1577af73d6e712eb113858b05ca5
JRoTheGeek <jvrosenman@gmail.com>
2013-09-20 23:09:31 -0400

    Added more of Neil's edits

--- Project2/new_syscall2.c
+++ Project2/new_syscall2.c
@@ -5,24 +5,10 @@
 #include <linux/time.h>
 #include <asm/cputime.h>
 #include <asm/errno.h>
+#include "processinfo.h"
 
 unsigned long **sys_call_table;
 asmlinkage long (*ref_sys_cs3013_syscall2)(void);
-
-struct processinfo {
-    long state;             // current state of process
-    pid_t pid;              // process ID of this process
-    pid_t parent_pid;       // process ID of parent
-    pid_t youngest_child;   // process ID of youngest child
-    pid_t younger_sibling;  // pid of next younger sibling
-    pid_t older_sibling;    // pid of next older sibling
-    uid_t uid;              // user ID of process owner
-    long long start_time;   // process start time in nanoseconds since boot time
-    long long user_time;    // CPU time in user mode (microseconds)
-    long long sys_time;     // CPU time in system mode (microseconds)
-    long long cutime;       // user time of children (microseconds)
-    long long cstime;       // system time of children (microseconds)
-};
 
 asmlinkage long new_sys_cs3013_syscall2(struct processinfo *info) {
     struct task_struct *current_task;
