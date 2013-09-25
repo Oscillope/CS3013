55367f78f2add3e520196bfa9f74a5a1bac936d3
JRoTheGeek <jvrosenman@gmail.com>
2013-09-25 14:18:45 -0400

    Ninjas and pirates alternate turns.

--- Project3/problem1.c
+++ Project3/problem1.c
@@ -1,28 +1,152 @@
+#include <errno.h>
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #define NUM_PIRATES 18
 #define NUM_NINJAS 14
+#define EMPTY 0
+#define PIRATE 1
+#define NINJA 2
 
 pthread_mutex_t chair1;
 pthread_mutex_t chair2;
+pthread_mutex_t state_mutex;
+pthread_mutex_t room_lock;
+pthread_cond_t room_cv;
+int room_state;
+
+void pirate(void);
+void ninja(void);
 
 int main(int argc, char** argv) {
-	pthread_mutex_init(chair1);
-	pthread_mutex_init(chair2);
-	pthread_t* pirates[NUM_PIRATES];
-	pthread_t* ninjas[NUM_NINJAS];
-	
+	pthread_mutex_init(&chair1, NULL);
+	pthread_mutex_init(&chair2, NULL);
+	pthread_mutex_init(&state_mutex, NULL);
+	pthread_mutex_init(&room_lock, NULL);
+	pthread_cond_init(&room_cv, NULL);
+	pthread_t pirate_threads[NUM_PIRATES];
+	pthread_t ninja_threads[NUM_NINJAS];
+	pthread_attr_t attr;
+	room_state = EMPTY;
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+	int i;
+	for(i = 0; i < NUM_PIRATES; i++)
+		pthread_create(&pirate_threads[i], &attr, (void*)&pirate, NULL);
+	for(i = 0; i < NUM_NINJAS; i++)
+		pthread_create(&ninja_threads[i], &attr, (void*)&ninja, NULL);
+	pthread_join(pirate_threads[0], NULL);
 	return 0;
 }
 
-int pirate() {
+void pirate(void) {
 	printf("AAAARRRRGHH\n");
-	
+	while(1) {
+		if(pthread_mutex_trylock(&state_mutex) != EBUSY) {
+			if(room_state == EMPTY)
+				room_state = PIRATE;
+			pthread_mutex_unlock(&state_mutex);
+		}
+		if(room_state == PIRATE) {
+			if(pthread_mutex_trylock(&chair1) != EBUSY) {
+				printf("Getting a costume in chair 1.\n");
+				sleep(2);
+				printf("Done!\n");
+				if(pthread_mutex_trylock(&chair2) != EBUSY) {
+					printf("I'm the last one here!\n");
+					pthread_mutex_lock(&state_mutex);
+					room_state = NINJA;
+					pthread_mutex_unlock(&state_mutex);
+					pthread_mutex_unlock(&chair2);
+				}
+				pthread_mutex_unlock(&chair1);
+				pthread_cond_broadcast(&room_cv);
+			}
+			else if(pthread_mutex_trylock(&chair2) != EBUSY) {
+				printf("Getting a costume in chair 2.\n");
+				sleep(2);
+				printf("Done!\n");
+				if(pthread_mutex_trylock(&chair1) != EBUSY) {
+					printf("I'm the last one here!\n");
+					pthread_mutex_lock(&state_mutex);
+					room_state = NINJA;
+					pthread_mutex_unlock(&state_mutex);
+					pthread_mutex_unlock(&chair1);
+				}
+				pthread_mutex_unlock(&chair2);
+				pthread_cond_broadcast(&room_cv);
+			}
+			else {
+				pthread_mutex_lock(&room_lock);
+				printf("zzzzz\n");
+				pthread_cond_wait(&room_cv, &room_lock);
+				pthread_mutex_unlock(&room_lock);
+				printf("I'm awake!\n");
+			}
+		}
+		else {
+			pthread_mutex_lock(&room_lock);
+			printf("This isn't our room!\n");
+			pthread_cond_wait(&room_cv, &room_lock);
+			pthread_mutex_unlock(&room_lock);
+		}
+		sleep(1);
+	}
 }
 
-int ninja() {
+void ninja(void) {
 	printf("...\n");
-	
+	while(1) {
+		if(pthread_mutex_trylock(&state_mutex) != EBUSY) {
+			if(room_state == EMPTY)
+				room_state = NINJA;
+			pthread_mutex_unlock(&state_mutex);
+		}
+		if(room_state == NINJA) {
+			if(pthread_mutex_trylock(&chair1) != EBUSY) {
+				printf("Embodying the night in chair 1.\n");
+				sleep(2);
+				printf("It is finished.\n");
+				if(pthread_mutex_trylock(&chair2) != EBUSY) {
+					printf("My people need me!\n");
+					pthread_mutex_lock(&state_mutex);
+					room_state = PIRATE;
+					pthread_mutex_unlock(&state_mutex);
+					pthread_mutex_unlock(&chair2);
+				}
+				pthread_mutex_unlock(&chair1);
+				pthread_cond_broadcast(&room_cv);
+			}
+			else if(pthread_mutex_trylock(&chair2) != EBUSY) {
+				printf("Embodying the night in chair 2.\n");
+				sleep(2);
+				printf("It is finished.\n");
+				if(pthread_mutex_trylock(&chair1) != EBUSY) {
+					printf("My people need me!\n");
+					pthread_mutex_lock(&state_mutex);
+					room_state = PIRATE;
+					pthread_mutex_unlock(&state_mutex);
+					pthread_mutex_unlock(&chair1);
+				}
+				pthread_mutex_unlock(&chair2);
+				pthread_cond_broadcast(&room_cv);
+			}
+			else {
+				pthread_mutex_lock(&room_lock);
+				printf("........\n");
+				pthread_cond_wait(&room_cv, &room_lock);
+				pthread_mutex_unlock(&room_lock);
+			}
+		}
+		else {
+			pthread_mutex_lock(&room_lock);
+			printf("SEPPUKU\n");
+			pthread_cond_wait(&room_cv, &room_lock);
+			pthread_mutex_unlock(&room_lock);
+			printf("I have been awoken.\n");
+		}
+		sleep(1);
+	}
 }
