d4a11bd40ab7a89a590eb3d225c3e32b0aa3c372
JRoTheGeek <jvrosenman@gmail.com>
2013-09-30 21:15:52 -0400

    Added linked lists for each feeder road.

--- Project3/problem2.c
+++ Project3/problem2.c
@@ -6,13 +6,33 @@
 #include <unistd.h>
 
 #define NUM_CARS 20
+#define LEFT 0
+#define STRAIGHT 1
+#define RIGHT 2
 
 sem_t road_nw;
 sem_t road_ne;
 sem_t road_sw;
 sem_t road_se;
 
-void car(int *car_num);
+void masshole(void);
+void add_car(car* head, car* add);
+
+typedef struct {
+	pthread_t thread;
+	char turn;
+	car* next;
+} car;
+
+// Initialize us some linked lists
+car north_cars;
+north_cars.next = &north_cars;
+car east_cars;
+east_cars.next = &east_cars;
+car south_cars;
+south_cars.next = &south_cars;
+car west_cars;
+west_cars.next = &west_cars;
 
 int main(int argc, char** argv) {
     sem_init(&road_nw, 0, 1);
@@ -25,39 +45,51 @@ int main(int argc, char** argv) {
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 	int i;
 	for(i = 0; i < NUM_CARS; i++)
-		pthread_create(car_threads[i], &attr, (void*)&car, &i);
-	pthread_join(car_threads[0], NULL);
+		pthread_create(&car_threads[i], &attr, (void*)&masshole, NULL);
+	pthread_join(car_threads[NUM_CARS-1], NULL);
 	return 0;
 }
 
-void car(int *car_num) {
+void masshole(void) {
     int random = rand();
     int randRoad = random % 4;
     int randDirection = random % 3;
-
+    car this_car;
+    this_car.thread = pthread_self();
+    this_car.turn = randDirection;
+    unsigned int car_num = (unsigned int)this_car.thread;
     switch(randRoad) {
         case 0:
-            printf("Car #%d entering from the north!\n", *car_num);
-	    break;
+            printf("Car #%u entering from the north!\n", car_num);
+            add_car(&north_cars, &this_car);
+			break;
         case 1:
-            printf("Car #%d entering from the east!\n", *car_num);
+            printf("Car #%u entering from the east!\n", car_num);
+            add_car(&east_cars, &this_car);
             break;
         case 2:
-            printf("Car #%d entering from the south!\n", *car_num);
+            printf("Car #%u entering from the south!\n", car_num);
+            add_car(&south_cars, &this_car);
             break;
         case 3:
-            printf("Car #%d entering from the west!\n", *car_num);
+            printf("Car #%u entering from the west!\n", car_num);
+            add_car(&west_cars, &this_car);
             break;
     }
     switch(randDirection) {
         case 0:
-            printf("Car #%d going left!\n", *car_num);
+            printf("Car #%u going left!\n", car_num);
             break;
         case 1:
-            printf("Car #%d going straight!\n", *car_num);
+            printf("Car #%u going straight!\n", car_num);
             break;
         case 2:
-            printf("Car #%d going right!\n", *car_num);
+            printf("Car #%u going right!\n", car_num);
             break;
     }
+}
+
+void add_car(car* head, car* add) {
+	add->next = head->next;
+	head->next = add;
 }
