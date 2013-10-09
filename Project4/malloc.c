eecb3d8dffca0cb2da1f536cd6a8e51266da6bc4
JRoTheGeek <jvrosenman@gmail.com>
2013-10-09 03:06:44 -0400

    Malloc now includes the header information it's supposed to, but does so badly and needs to be fixed.

--- Project4/malloc.c
+++ Project4/malloc.c
@@ -1,30 +1,48 @@
 //Jason Rosenman & Neil Pomerleau
+//TODO: Instead of making the pointers a char* and casting to int*, make a void* and cast to char* for arithmetic.
 
 #include "malloc.h"
 
-void* head = NULL;
-void* next = NULL;
+char* head = NULL;
+char* cur = NULL;
 
-int chunk_size = 256;
+int chunk_size = 0;
+int free_space = 0;
 
 void* malloc(size_t size) {
+	cur = head;
 	if(head == NULL) {
-		head = sbrk(256);
-		next = head;
+		chunk_size = size * 10;
+		head = sbrk(chunk_size);
+		cur = head;
+		*(int*)cur = chunk_size;
+		*(cur + 4) = NULL;	//No next yet.
+		*(cur + 8) = NULL;	//The beginning of the list should never have a previous.
 	}
-	else if(next + size >= head + chunk_size) {
-		chunk_size += 256;
-		next = sbrk(256);
+	else if(*(int*)cur == 0) {
+		while(*(cur + 4) != NULL && *(int*)cur == 0) cur = *(int*)(cur + 4);
+		if(cur + size + 32 >= head + chunk_size) {
+			chunk_size += size * 10;
+			cur = sbrk(size * 10);
+			*(int*)cur = size * 10;
+			*(cur + 4) = NULL;
+		}
 	}
-	else
-		next += (size + 8);
-	int* mem_size = (int*)next;
-	*mem_size = size;		//Put size in beginning of block.
-	next += 4;
-	int* checksum = (int*)next;
-	*checksum = 1;			//There will be a checksum here eventually.
-	next += 4;
-	return next;
+	free_space = *(int*)cur - size - 20;
+	*(int*)cur = 0;					//We're going to write here, so the block is no longer free.
+	#ifdef DEBUG
+		printf("My block starts at %p, I'm going to give the process a %d byte block starting at %p.\n", cur, size, (cur + 20));
+	#endif
+	*(int*)(cur + 4) = cur + size + 20;	//Set the next pointer to where the next header is going to be.
+	*(int*)(cur + size + 20) = free_space;
+	*(cur + size + 24) = NULL;
+	*(cur + size + 28) = cur;		//Set the next header's previous pointer to where we are now.
+	cur += 12;						//Get out of the header.
+	*(int*)cur = size;				//Put size in beginning of block.
+	cur += 4;
+	*(int*)cur = 1;					//There will be a checksum here eventually.
+	cur += 4;
+	return (void*)cur;
 }
 
 void free(void* ptr) {
