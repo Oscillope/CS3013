6ee3d338011043776cd092c4e7043801bbf860c6
Jason Rosenman <os@Bert>
2013-10-11 02:39:13 -0400

    Appears to be working, however whenever I try to print a pointer address it calls free on the VM.

--- Project4/stress_malloc.c
+++ Project4/stress_malloc.c
@@ -1,9 +1,9 @@
 //Jason Rosenman & Neil Pomerleau
 
-#include "malloc.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "malloc.h"
 
 int main(int argc, char** argv) {
 	int i = 0;
@@ -11,12 +11,14 @@ int main(int argc, char** argv) {
 	char* teststring2;
 	while(1) {
 		teststring = (char*)malloc(strlen("Hello, world!")+1);
-		strncpy(teststring, "Hello, world!", strlen("Hello, world!") + 1);
-		printf("%s, %d @ %p\n", teststring, strlen(teststring), teststring);
+		strcpy(teststring, "Hello, world!");
+		//printf("%s @ %p\n", teststring, teststring);
+		printf("%s\n", teststring);
 		free(teststring);
 		teststring2 = (char*)malloc(strlen("Something else!")+1);
 		strncpy(teststring2, "Something else!", strlen("Something else!")+1);
-		printf("%s @ %p, %d\n", teststring2, teststring2, i);
+		//printf("%s @ %p, %d\n", teststring2, teststring2, i);
+		printf("%s\n", teststring);
 		free(teststring2);
 		//sleep(1);
 		i++;
